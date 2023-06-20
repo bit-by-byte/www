a9f4483adbacb82806bc4137b53b418c4d222eda
Ayush <61765149+citricacid2@users.noreply.github.com>
2023-06-19 23:29:22 -0400

    partially tailwindified?

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,327 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+// import bulma
+import "bulma/css/bulma.min.css";
+import Typed from "typed.js";
+import { useEffect } from "react";
+import { FaEnvelope, FaInstagram, FaTwitter } from "react-icons/fa";
+import Link from "next/link";
+import logo from "@/public/bbblogo.svg";
 
 export default function Home() {
+  useEffect(() => {
+    let typed = new Typed(".typed", {
+      stringsElement: ".typed-phrases",
+      // showCursor: false,
+      smartBackspace: true,
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 1000,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="">
+      <section className="tw-h-screen tw-flex tw-flex-col">
+        <div className="hero-head">
+          <nav className="navbar">
+            <div className="container">
+              <div className="navbar-brand">
+                <a href="/" className="navbar-item">
+                  {/* <img src="/images/logo2.2.svg" /> */}
+                  <Image src={logo} height={200} alt="Bit by Byte Logo" />
+                </a>
+
+                <a
+                  role="button"
+                  className="navbar-burger"
+                  aria-label="menu"
+                  aria-expanded="false"
+                  data-target="navMenu"
+                >
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                </a>
+              </div>
+              <div className="navbar-menu" id="navMenu">
+                <div className="navbar-end">
+                  <a href="/" className="navbar-item">
+                    Home
+                  </a>
+                  <div className="navbar-item">
+                    <a href="#classes" className="button is-rounded is-danger">
+                      Classes
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <div className="tw-flex-grow tw-flex tw-flex-col tw-justify-center">
+          <div className="tw-container tw-text-center tw-mx-auto ">
+            <div className="typed-phrases">
+              <p>Welcome to Coding!</p>
+              <p>
+                Welcome to{" "}
+                <span className="tw-bg-gradient-to-r tw-from-orange-400 tw-to-red-500 tw-text-transparent tw-bg-clip-text">
+                  Bit by Byte!
+                </span>
+              </p>
+            </div>
+            <div className="tw-text-[6vw] tw-mb-6 tw-font-bold">
+              <div className="typed is-inline"></div>
+            </div>
+            <h1 className="subtitle">
+              Get ready for the start of your programming adventure!
+            </h1>
+          </div>
+        </div>
+        <Socials />
+      </section>
+      <main>
+        <div className="section" id="initiatives">
+          <div className="content is-medium">
+            <h1>Initiatives</h1>
+            <p>Check out what we're planning!</p>
+          </div>
+          <div className="columns">
+            <div className="column">
+              <div className="card">
+                <div className="card-header">
+                  <div className="card-header-title">queen_city_hacks</div>
+                </div>
+                <div className="card-content">
+                  <div className="content">
+                    <p>
+                      <a href="https://www.queencityhacks.com">
+                        Queen City Hacks
+                      </a>
+                      is a 12-hour, in-person, high-school hackathon that
+                      celebrates the creativity and innovation of Charlotte’s
+                      high school students.
+                    </p>
+                    <p>
+                      We plan to host the{" "}
+                      <strong>biggest high school hackathon</strong> in the
+                      Greater Charlotte Area.
+                    </p>
+                  </div>
+                </div>
+                <div className="card-footer">
+                  <div className="card-footer-item has-text-grey">
+                    Percent Completed
+                    <progress className="progress" value="60" max="100">
+                      60%
+                    </progress>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div className="card">
+                <div className="card-header">
+                  <div className="card-header-title">Coming Soon</div>
+                </div>
+                <div className="card-content">
+                  <div className="content">...</div>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div className="card">
+                <div className="card-header">
+                  <div className="card-header-title">Coming Soon</div>
+                </div>
+                <div className="card-content">
+                  <div className="content">...</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="section" id="classes">
+          <div className="content is-medium">
+            <h1>Upcoming Classes</h1>
+            <p>See what we have in store!</p>
+          </div>
+          <div className="columns">
+            <div className="column">
+              <div className="card">
+                <div className="card-header">
+                  <div className="card-header-title">
+                    Make a Game with Scratch!
+                  </div>
+                </div>
+                <div className="card-content">
+                  <div className="content">
+                    <p>
+                      <a href="https://scratch.mit.edu/">Scratch</a> is a basic
+                      block-based programming langange. In this class, we will
+                      learn the basics of Scratch by programming our own game!
+                    </p>
+                    <p>
+                      This course is <strong>highly recommended</strong> to take
+                      first if you are new to programming.
+                    </p>
+                  </div>
+                </div>
+                <div className="card-footer">
+                  <div className="card-footer-item has-text-grey">
+                    Curriculum in Development
+                    <progress className="progress" value="20" max="100">
+                      20%
+                    </progress>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div className="card">
+                <div className="card-header">
+                  <div className="card-header-title">Python Programming</div>
+                </div>
+                <div className="card-content">
+                  <div className="content">
+                    <p>
+                      <a href="https://www.python.org/">Python</a> is a popular
+                      programming language used in many applications. In this
+                      class, we will learn the basics of Python by programming
+                      our own game!
+                    </p>
+                    <p>
+                      If you do not have prior experience with programming, we
+                      recommend taking Make a Game with Scratch first.
+                    </p>
+                  </div>
+                </div>
+                <div className="card-footer">
+                  <div className="card-footer-item has-text-grey">
+                    Curriculum in Development
+                    <progress className="progress" value="65" max="100">
+                      65%
+                    </progress>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div className="card">
+                <div className="card-header">
+                  <div className="card-header-title">Coming Soon</div>
+                </div>
+                <div className="card-content">
+                  <div className="content">...</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer className="footer">
+        <div className="content has-text-centered">
+          <p>
+            <strong>Bit by Byte</strong> is a NC Nonprofit Organization.
+            <br />
+            Copyright © 2021 Bit by Byte under the
+            <a href="https://raw.githubusercontent.com/bit-by-byte/bit-by-byte.github.io/master/LICENSE">
+              MIT License.
+            </a>
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        </div>
+      </footer>
     </main>
-  )
+  );
+}
+
+function Header() {
+  return (
+    <div className="hero-head">
+      <nav className="navbar">
+        <div className="container">
+          <div className="navbar-brand">
+            <a href="/" className="navbar-item">
+              {/* <img src="/images/logo2.2.svg" /> */}
+              <Image src={logo} height={200} alt="Bit by Byte Logo" />
+            </a>
+
+            <a
+              role="button"
+              className="navbar-burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navMenu"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+          <div className="navbar-menu" id="navMenu">
+            <div className="navbar-end">
+              <a href="/" className="navbar-item">
+                Home
+              </a>
+              <div className="navbar-item">
+                <a href="#classes" className="button is-rounded is-danger">
+                  Classes
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+function Hero() {
+  return (
+    <div className="flex-grow">
+      <div className="container has-text-centered">
+        <div className="typed-phrases">
+          <p>Welcome to Coding!</p>
+          <p>
+            Welcome to <span className="orange">Bit by Byte!</span>
+          </p>
+        </div>
+        <div className="title mb-6">
+          <div className="typed is-inline"></div>
+        </div>
+        <h1 className="subtitle">
+          Get ready for the start of your programming adventure!
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+function Socials() {
+  return (
+    <div className="hero-foot">
+      <div className="social-bar columns has-text-centered mb-6">
+        <div className="column"></div>
+        <div className="column narrow orange !tw-flex tw-flex-row tw-justify-center tw-text-xl">
+          <Link href="https://www.instagram.com/bit.by.byte/">
+            {/* <i className="fab fa-instagram" aria-hidden="true"></i> */}
+            <FaInstagram />
+          </Link>
+          <Link href="https://twitter.com/bitbybyteinc">
+            {/* <i className="fab fa-twitter" aria-hidden="true"></i> */}
+            <FaTwitter />
+          </Link>
+          <Link href="mailto:ayush@bitbybyte.org">
+            {/* <i className="far fa-envelope" aria-hidden="true"></i> */}
+            <FaEnvelope />
+          </Link>
+        </div>
+        <div className="column"></div>
+      </div>
+    </div>
+  );
 }
